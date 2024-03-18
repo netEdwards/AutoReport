8f6c1dc33db73450fae8cb52e5fbe086c7d23e07
Adrian Edwards <aedwards0603@gmail.com>
2024-03-18 16:06:05 -0400

    Changes to be committed: 	new file:   __pycache__/collect.cpython-312.pyc 	new file:   __pycache__/functions.cpython-312.pyc 	modified:   collect.py 	new file:   functions.py 	new file:   mockRequestdata/payloads 	new file:   page.html 	new file:   screenshot7.png
    Changed to approach from copying data from the html elements to pulling the data from a XHR request.

--- collect.py
+++ collect.py
@@ -3,10 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import *
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from functions import updateCookieFromSelenium, getTokens
 import time
-import csv
+import requests
+import re
+import json
 
-driver = webdriver.Chrome()
+
+chrome_options = Options()
+chrome_options.add_argument("--ignore-certificate-errors")
+
+driver = webdriver.Chrome(options=chrome_options)
+
 
 driver.get("https://chick-fil-a.compliancemetrix.com/rql/p/acfareportsallreportsvtopissuesactivator")
 driver.implicitly_wait(2)
@@ -22,26 +31,38 @@ verify_button = driver.find_element(By.CLASS_NAME, 'button-primary') #find the n
 driver.implicitly_wait(1)
 verify_button.click() # click the next/verify.
 time.sleep(10)
-
+#-------SET LOCATION-------#
 combobox = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[3]/div/div[4]/div/div[3]/div/div/div/div[1]/form[2]/div[2]/div/div[2]/div[1]/div[1]/div[5]/div/div/div')
 combobox.click()
 comboSelection = driver.find_element(By.XPATH, '/html/body/span[3]/span/span/ul/li[3]')
 comboSelection.click()
 driver.refresh()
-time.sleep(10)
+time.sleep(2)
+#-------FIND TABLE AND ROWS-------#
+wait = WebDriverWait(driver, 10)
+table = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'table')))
+rows = table.find_elements(By.TAG_NAME, 'tr')
 
-table = driver.find_element(By.CSS_SELECTOR, 'table')
-rows  = driver.find_elements(By.CSS_SELECTOR, 'tr')
+#-------DATA VAR FOR STORING RESPONSE-------#
+session = requests.Session()
+xhr_url = "https://chick-fil-a.compliancemetrix.com/rql/execute"
 
-riskValues = []
-for row in rows:
-    # hiRisk = row.find_element(By.CSS_SELECTOR, 'td[data-header="# HIGH RISK"]')
-    hiRisk = row.find_element(By.XPATH, '//*[@id="r171051715143367436"]/div[1]/div/div/table/tbody/tr[1]/td[4]')
-    hiRiskValue = hiRisk.find_element(By.TAG_NAME, 'span')
-    if hiRiskValue.text:
-        riskValues.append(hiRiskValue.text)
-        print(hiRiskValue.text)
-    
+#-------LOOP THROUGH TABLE AND CLICK ON VIEW-------#
+for i in range(len(rows)-1):
+    row = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'table tbody tr')))[i]
+    view_link = row.find_element(By.CSS_SELECTOR, 'a')
+    driver.execute_script("arguments[0].click();", view_link)
+    time.sleep(3)
+#-------GET TOKENS AND SEND XHR REQUEST-------#
+    updateCookieFromSelenium(driver, session)
+    headers, data = getTokens(driver)
+    response = session.post(xhr_url, headers=headers, json=data)
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print("Failed to retrieve data via XHR replication", response.status_code, response.text)
+
 
     #PROBLEM
     #If we use the loop to click on view and retrieve the finding for the question, then it will leave the current DOM probbaly  fuckin up everything. Making the loop kinda useless.
